9a26caa890e0f1d5ef63316a1246d94bb9d4efc9
akkomar <akkomar@users.noreply.github.com>
2015-05-25 13:01:16 +0200

    Updated app

--- build.sbt
+++ build.sbt
@@ -5,8 +5,11 @@ version := "1.0"
 scalaVersion := "2.11.6"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.4-M1",
+  "com.typesafe.akka" %% "akka-actor" % "2.3.11",
   "io.kamon" %% "kamon-core" % "0.4.0",
-  "io.kamon" %% "kamon-statsd" % "0.4.0"
+  "io.kamon" %% "kamon-log-reporter" % "0.4.0",
+  "io.kamon" %% "kamon-statsd" % "0.4.0",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
+  "ch.qos.logback" %  "logback-classic" % "1.1.2"
 )
     
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,16 @@
-import java.util
 import java.util.concurrent._
 
+import akka.actor.ActorSystem
+import com.typesafe.scalalogging.LazyLogging
 import kamon.Kamon
+import kamon.trace.Tracer
 
-object Main extends App {
+import scala.concurrent.duration.Duration
+
+object Main extends App with LazyLogging {
   Kamon.start()
+
+  val r = scala.util.Random
 
   val someHistogram = Kamon.metrics.histogram("some-histogram")
   val someCounter = Kamon.metrics.counter("some-counter")
@@ -13,7 +19,40 @@ object Main extends App {
   someHistogram.record(50)
   someCounter.increment()
 
-  // This application wont terminate unless you shutdown Kamon.
-  Kamon.shutdown()
 
+  val actorSystem = ActorSystem()
+  val scheduler = actorSystem.scheduler
+  val bigTask = new Runnable {
+    def run() {
+      someCounter.increment()
+      Tracer.withNewContext("bigTask-trace", autoFinish = true) {
+        Tracer.currentContext.withNewSegment("first-segment", "segment-category", "segment-library") {
+          Thread.sleep(r.nextInt(100))
+          logger.info("First step")
+        }
+
+        Tracer.currentContext.withNewSegment("second-segment", "segment-category", "segment-library") {
+          Thread.sleep(r.nextInt(300))
+          logger.info("Second step")
+        }
+
+        Tracer.currentContext.withNewSegment("third-segment", "segment-category", "segment-library") {
+          Thread.sleep(r.nextInt(50))
+          logger.info("Final step")
+        }
+      }
+
+    }
+  }
+
+  implicit val executor = actorSystem.dispatcher
+
+  scheduler.schedule(
+    initialDelay = Duration(0, TimeUnit.SECONDS),
+    interval = Duration(50, TimeUnit.MILLISECONDS),
+    runnable = bigTask)
+
+  Thread.sleep(50000)
+  actorSystem.shutdown()
+  Kamon.shutdown()
 }
