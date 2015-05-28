f03ffe60c29127d93dac75591c6211d9f60d8a13
akkomar <ak.komar@gmail.com>
2015-05-28 23:15:07 +0200

    Exposed Graphite port on docker container

--- run-docker.sh
+++ run-docker.sh
@@ -4,4 +4,4 @@ docker kill kamon-grafana-example
 docker rm kamon-grafana-example
 truncate ./logs/* --size 0
 
-docker run -d -v $(pwd)/logs:/var/log/supervisor -p 80:80 -p 8125:8125/udp -p 8126:8126 --name kamon-grafana-example akomar/kamon-grafana-example
+docker run -d -v $(pwd)/logs:/var/log/supervisor -p 80:80 -p 81:81 -p 8125:8125/udp -p 8126:8126 --name kamon-grafana-example akomar/kamon-grafana-example
